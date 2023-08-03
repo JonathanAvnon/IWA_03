20c3fe264b68799b310d061e3b3a30ea4ad4fc77
JonathanAvnon <info@josephavnon.com>
2023-08-03 18:11:03 +0200

    IWA_03 Challenge 1 - Not working

---
+++ scripts.js
@@ -0,0 +1,8 @@
+import {company} from "./configuration"
+import {year} from "./configuration"
+
+const copyright = © 
+
+const message = 'copyright' + 'company' + 'year'
+document.querySelector('footer').innerText = message
+
